b62d5f2eed8421a0e22256e9de37e9fee473df4b
Tim van Dijk <timvan@amazon.com>
2017-02-16 14:47:05 -0800

    Updated test.

--- test/benchmark/one_publisher_to_one_unicast_throughput_test.cc
+++ test/benchmark/one_publisher_to_one_unicast_throughput_test.cc
@@ -22,72 +22,62 @@
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-#include <sys/time.h>
-
-#include <exception>
-#include <functional>
 #include <iostream>
-#include <memory>
 #include <thread>
 
 #include <disruptor/ring_buffer.h>
-#include <disruptor/event_publisher.h>
-#include <disruptor/event_processor.h>
-#include <disruptor/exception_handler.h>
-
-#include "../test/support/stub_event.h"
+#include <disruptor/sequencer.h>
 
 using namespace disruptor;
 
-int main(int arc, char** argv) {
-    int buffer_size = 1024 * 8;
-    long iterations = 1000L * 1000L * 300;
+namespace
+{
+    const size_t ringBufferSize = 1024 * 8;
+    const int64_t iterations = 1000L * 1000L * 300;
+}
 
-    test::StubEventFactory stub_factory;
-    RingBuffer<test::StubEvent> ring_buffer(&stub_factory,
-                                       buffer_size,
-                                       kSingleThreadedStrategy,
-                                       kBusySpinStrategy);
+int main(int arc, char** argv)
+{
+    std::array<int64_t, ringBufferSize> buffer{};
+    Sequencer<int64_t, ringBufferSize, SingleThreadedStrategy<ringBufferSize>, BusySpinStrategy> sequencer(buffer);
 
-    std::vector<Sequence*> sequence_to_track(0);
-    std::unique_ptr<ProcessingSequenceBarrier> barrier(
-        ring_buffer.NewBarrier(sequence_to_track));
+    auto barrier = sequencer.NewBarrier({});
 
-    test::StubBatchHandler stub_handler;
-    IgnoreExceptionHandler<test::StubEvent> stub_exception_handler;
-    BatchEventProcessor<test::StubEvent> processor(&ring_buffer,
-                                              (SequenceBarrierInterface*) barrier.get(),
-                                              &stub_handler,
-                                              &stub_exception_handler);
+    Sequence consumerSequence;
+    sequencer.set_gating_sequences({ &consumerSequence });
 
-    std::thread consumer(std::ref<BatchEventProcessor<test::StubEvent>>(processor));
+    std::thread consumer([&barrier, &sequencer, &consumerSequence]
+    {
+        int64_t nextSequence = consumerSequence.sequence() + 1;
+        while (nextSequence < iterations)
+        {
+            auto availableSequence = barrier.WaitFor(nextSequence);
+            if (availableSequence >= disruptor::kInitialCursorValue)
+            {
+                nextSequence = availableSequence;
+                consumerSequence.set_sequence(nextSequence);
+            }
+        }
+    });
 
-    struct timeval start_time, end_time;
+    auto start = std::chrono::steady_clock::now();
 
-    gettimeofday(&start_time, NULL);
-
-    std::unique_ptr<test::StubEventTranslator> translator(new test::StubEventTranslator);
-    EventPublisher<test::StubEvent> publisher(&ring_buffer);
-    for (long i=0; i<iterations; i++) {
-        publisher.PublishEvent(translator.get());
+    for (int64_t i = 0; i < iterations; i++)
+    {
+        auto sequence = sequencer.Claim(1);
+        sequencer[sequence] = i;
+        sequencer.Publish(sequence);
     }
 
-    long expected_sequence = ring_buffer.GetCursor();
-    while (processor.GetSequence()->sequence() < expected_sequence) {}
+    int64_t expected_sequence = sequencer.GetCursor();
+    while (consumerSequence.sequence() < expected_sequence) {}
 
-    gettimeofday(&end_time, NULL);
-
-    double start, end;
-    start = start_time.tv_sec + ((double) start_time.tv_usec / 1000000);
-    end = end_time.tv_sec + ((double) end_time.tv_usec / 1000000);
+    auto end = std::chrono::steady_clock::now();
 
     std::cout.precision(15);
     std::cout << "1P-1EP-UNICAST performance: ";
-    std::cout << (iterations * 1.0) / (end - start)
-              << " ops/secs" << std::endl;
+    std::cout << iterations / std::chrono::duration<double, std::ratio<1>>(end - start).count() << " ops/secs" << std::endl;
 
-    processor.Halt();
     consumer.join();
 
     return EXIT_SUCCESS;
